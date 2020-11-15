fd33ddcff8084502a82180405aa3e79ce9e99d81
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-14 16:17:44 -0800

    able to share and save photo

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -18,6 +18,8 @@ class DetailViewController: UIViewController {
         navigationItem.largeTitleDisplayMode = .never
         // Do any additional setup after loading the view.
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage{
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -36,6 +38,18 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped(){
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else{
+            print("Image not found!")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 
